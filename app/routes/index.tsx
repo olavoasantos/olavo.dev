806152e233a50cceae828ab30d8a815278f35f34
Olavo Amorim Santos <olavo.a.santos@gmail.com>
2022-10-01 17:52:28 -0500

    fix: removed signup button

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -38,12 +38,6 @@ export default function Index() {
                 ) : (
                   <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
                     <Link
-                      to="/join"
-                      className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-blue-700 shadow-sm hover:bg-blue-50 sm:px-8"
-                    >
-                      Sign up
-                    </Link>
-                    <Link
                       to="/login"
                       className="flex items-center justify-center rounded-md bg-blue-500 px-4 py-3 font-medium text-white hover:bg-blue-600"
                     >
